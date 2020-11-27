2d8a02a833bb0926c1f8ecf0487ac658decc4d50
cryptoBLD <33181015+cryptoBLD@users.noreply.github.com>
2020-11-27 19:56:58 +0100

    Update guesser.py

--- guesser.py
+++ guesser.py
@@ -1,16 +1,22 @@
 import random
 import easygui
 tries = 0
+guesses = 6
 number = random.randint(1, 100)
 awnser = easygui.integerbox("Number between 1 and 100, what's your guess?")
 
-while int(awnser) != number:
+while int(awnser) != number and guesses != 0:
     tries = tries + 1
     if int(awnser) > number:
         easygui.msgbox("Too high, try again")
         awnser = easygui.integerbox("Awnser:")
+        guesses -= 1
     elif int(awnser) < number:
         easygui.msgbox("Too low, try again")
         awnser = easygui.integerbox("Awnser:")
+        guesses -= 1
 tries = tries + 1
-easygui.msgbox("Correct!, you had " + str(tries) + " tries")
+if guesses != 0:
+    easygui.msgbox("Correct!, you had " + str(tries) + " tries")
+else:
+    easygui.msgbox("You failed idiot, the awnser was " + str(number))
